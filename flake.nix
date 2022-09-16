eebf9aaac3c77a347832e75fb268fa8bbcc9c903
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-15 21:35:30 -0400

    init flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "protohackers";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, gitignore, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion =
+          (pkgs.rust-bin.fromRustupToolchainFile ./rust/rust-toolchain.toml);
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+        proto-rust = rustPlatform.buildRustPackage {
+          pname = "bang";
+          version = "0.1.0";
+          src = gitignore.lib.gitignoreSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+      in with pkgs; {
+        defaultPackage = proto-rust;
+        devShell = mkShell {
+          buildInputs = [
+            (rustVersion.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+            cargo-expand
+            # common
+            watchexec
+
+            nixfmt
+          ];
+        };
+      });
+}
