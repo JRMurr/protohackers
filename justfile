ca4c6502d8557348a898951b67fa682f191565e6
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-15 23:11:12 -0400

    problem 0

--- justfile
+++ justfile
@@ -5,6 +5,9 @@ default:
 build:
     ./buildRustImage.sh
 
-
 deploy:
-    flyctl deploy --image proto-rust:latest --local-only 
+    flyctl deploy --image proto-rust:latest --local-only --strategy immediate
+
+buildDeploy:
+    just build
+    just deploy
--- rust/src/problem0.rs
+++ rust/src/problem0.rs
@@ -6,12 +6,11 @@ use tokio::net::TcpStream;
 #[derive(Copy, Clone, Debug)]
 pub struct EchoServer;
 
-
 #[async_trait]
 impl ProtoServer for EchoServer {
     async fn run_server(&self, mut socket: TcpStream) -> anyhow::Result<()> {
         let mut buf = [0; 1024];
-
+        // Read until the client closes the socket
         loop {
             let bytes = self.read(&mut socket, &mut buf).await?;
             self.write(&mut socket, bytes).await?;
